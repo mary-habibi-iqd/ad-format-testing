aaa98dc22c5b2f6dbf099c2459f51724f4a2a551
Mary Habibi <mary.habibi@iqdigital.de>
2024-04-23 21:10:20 +0200

    Implement dynamic link generation functionality

--- script.js
+++ script.js
@@ -1,76 +1,82 @@
+const mandanten = {
+    "Gesellschafter": {
+        FAZ: 'www.faz.net',
+        Handelsblatt: 'www.handelsblatt.com',
+        WirtschaftsWoche: 'www.wiwo.de',
+        Sueddeutsche: 'www.sueddeutsche.de',
+        SZMagazin: 'www.sz-magazin.sueddeutsche.de',
+        Jetzt: 'www.jetzt.de',
+        ZEITOnline: 'www.zeit.de'
+    },
+    "Premium Portfolio": {
+        Ariva: 'www.ariva.de',
+        Tagesspiegel: 'www.tagesspiegel.de',
+        SpiegelOnline: 'www.spiegel.de',
+        ManagerMagazin: 'www.manager-magazin.de',
+        ElfFreunde: 'www.11freunde.de',
+        Edison: 'www.edison.media',
+        BTCEcho: 'www.btc-echo.de',
+        DUSFlughafen: 'www.dus.com',
+        HamburgAirport: 'www.hamburg-airport.de',
+        SaechsischeZeitung: 'www.saechsische.de',
+        Spektrum: 'www.spektrum.de',
+        Monopol: 'www.monopol-magazin.de',
+        Cicero: 'www.cicero.de',
+        DerFreitag: 'www.freitag.de',
+        DEL2: 'www.del-2.org',
+        TheFan: 'www.thefan.fm',
+        DerPostillon: 'www.der-postillon.com',
+        Weltkunst: 'www.weltkunst.de',
+        Nautano: 'www.nautano.de'
+    },
+    "Special Interest - Erstvermarktung": {
+        TheEuropean: 'www.theeuropean.de',
+        BoerseAmSonntag: 'www.boerse-am-sonntag.de',
+        MarktUndMittelstand: 'www.marktundmittelstand.de',
+        Wissen: 'www.wissen.de',
+        Scinexx: 'www.scinexx.de',
+        Genios: 'www.genios.de',
+        ForschungUndWissen: 'www.forschung-und-wissen.de',
+        Tilasto: 'www.tilasto.de',
+        Damals: 'www.damals.de',
+        Natur: 'www.natur.de',
+        ScienceBlogs: 'www.scienceblogs.de',
+        Wirtschaftskurier: 'www.wirtschaftskurier.de',
+        Wissenschaft: 'www.wissenschaft.de'
+    }
+};
 
+const queryStart = 'iqadtest=iqdtests,';
+const output = document.querySelector('#output');
+const input = document.querySelector('#custom-params');
+const button = document.querySelector('#create-links');
 
-        const mandanten = {
-         
-            zeit: 'www.zeit.de',
-            faz: 'www.faz.net',
-            wirtschaftsWoche: 'www.wiwo.de',
-            sueddeutsche: 'www.sueddeutsche.de',
-            szMagazin: 'www.sz-magazin.sueddeutsche.de',
-            jetzt: 'www.jetzt.de',
-            handelsblatt: 'www.handelsblatt.com',
+button.addEventListener('click', () => {
+    output.innerHTML = '';
+    createLinks();
+});
 
-            wissen: 'www.wissen.de',
+function createLinks() {
+    const customquery = input.value;
+    const queryParams = queryStart + customquery;
 
-            ariva: 'www.ariva.de',
-            tagesSpiegel: 'www.tagesspiegel.de',
-            spiegel: 'www.spiegel.de',
-            managerMagazin: 'www.manager-magazin.de',
-            elfFreunde: 'www.11freunde.de',
-            edisonMedia: 'www.edison.media',
-            btcEcho: 'www.btc-echo.de',
-            DUS: 'www.dus.com',
-            HamburgerFlughafen: 'www.hamburg-airport.de',
-            sächsischeZeitung: 'www.saechsische.de',
-            spektrum: 'www.spektrum.de',
-            monopol: 'www.monopol-magazin.de',
-            cicero: 'www.cicero.de',
-            derFreitag: 'www.freitag.de',  
+    for (const [group, websites] of Object.entries(mandanten)) {
+        const header = document.createElement('h2');
+        header.textContent = group;
+        output.appendChild(header);
 
-            del2: 'www.del-2.org',
-            theFan: 'www.thefan.fm',
-            postillon: 'www.der-postillon.com',
-            weltkunst: 'www.weltkunst.de',
-            nautano: 'www.nautano.de',
+        const list = document.createElement('ul');
 
-            // special interest
-            theEuropean: 'www.theeuropean.de',
-            boerseAmSonntag: 'www.boerse-am-sonntag.de',
-            MarktUndMittelstand: 'www.marktundmittelstand.de',
-            wissen: 'www.wissen.de',
-            scinexx: 'www.scinexx.de',
-            genios: 'www.genios.de',
-            forschungUndWissen: 'www.forschung-und-wissen.de',
-            tilasto: 'www.tilasto.de',
-            damals: 'www.damals.de',
-            natur: 'www.natur.de',
-            scienceBlogs: 'www.scienceblogs.de',
-            wirtschaftsKurier: 'www.wirtschaftskurier.de',
-            wissenschaft: 'www.wissenschaft.de',
-    
-        } 
-
-        const queryStart = 'iqadtest=iqdtests,'
- 
-        const output = document.querySelector('output')
-        const input = document.querySelector('#custom-params')
-        const button = document.querySelector('#create-links')
-        button.addEventListener('click', () => {
-            output.innerHTML = ''
-            createLinks()
-        })
-        function createLinks() {
-            const customquery = input.value
-            const queryParams = queryStart + customquery
-            Object.entries(mandanten).forEach(([key, value]) => {
-                const anchor = document.createElement('a')
-                anchor.href = `https://${value}?${queryParams}`
-           
-                anchor.innerText = key
-                anchor.style.marginBottom = '10px'
-                anchor.target = '_blank'
-                output.appendChild(anchor)
-            })
+        for (const [website, url] of Object.entries(websites)) {
+            const listItem = document.createElement('li');
+            const anchor = document.createElement('a');
+            anchor.href = `https://${url}?${queryParams}`;
+            anchor.textContent = website;
+            anchor.target = '_blank';
+            listItem.appendChild(anchor);
+            list.appendChild(listItem);
         }
-       
- 
+
+        output.appendChild(list);
+    }
+}
